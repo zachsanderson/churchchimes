0cd5a837d35d64b2dd960c1aa57ccca529dbe55e
Zach Sanderson <zach@zachsanderson.com>
2016-05-11 14:20:19 -0500

    Initial commit

---
+++ chimeplayer.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var lame = require('lame');
+var Speaker = require('speaker');
+
+// TODO: parse schedule
+
+// TODO: setup timeouts
+setTimeout(function() {
+    console.log("starting");    
+    fs.createReadStream(process.argv[2])
+      .pipe(new lame.Decoder())
+      .on('format', function (format) {
+        this.pipe(new Speaker(format));
+      });
+    }, 60* 1000);
+
+console.log("done");
