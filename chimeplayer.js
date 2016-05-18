290e75d033f5188e2dc1e7a9c99f8fe31b586aed
Zach Sanderson <zach@zachsanderson.com>
2016-05-18 14:39:22 -0500

    Read in file, note when playback finished

--- chimeplayer.js
+++ chimeplayer.js
@@ -9,14 +9,17 @@ var songlist = fs.readFileSync("arminius.csv", "UTF-8");
 console.log(songlist);
 
 
+
 // TODO: setup timeouts
 setTimeout(function() {
     console.log("starting");    
-    fs.createReadStream(process.argv[2])
+    fs.createReadStream(`audio/${process.argv[2]}`)
       .pipe(new lame.Decoder())
       .on('format', function (format) {
         this.pipe(new Speaker(format));
-      });
+      })
+      .on("end", function() {console.log("Song finished playing");});
     }, 3 * 1000);
+
 
 console.log("done");
