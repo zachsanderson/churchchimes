80d96a11554777ae540fde61eece80104f938b0e
Zach Sanderson <zach@zachsanderson.com>
2016-05-18 20:37:16 -0500

    Songs playing in succession, instead of just one

--- chimeplayer.js
+++ chimeplayer.js
@@ -8,7 +8,8 @@ var songlist = fs.readFileSync("arminius.txt", "UTF-8"),
     lines = songlist.split("\n"),
     output = [],
     obj,
-    values;
+    values,
+    delay;
 
 for (i=0; i < lines.length; i++) {
     if (lines[i].length > 0) {
@@ -45,19 +46,40 @@ for (i=0; i < output.length; i++) {
 
 console.log(new Date(Date.now()).toString());
 
-var delay = new Date(output[0].timetoplay) - Date.now();
-console.log(`${output[0].filename} will play in ${delay/1000} seconds`); 
+if (output.length > 0) {
+    queueSong(output[0]);
+} else {
+    console.log("No more songs to play (did not enter function)");
+}
 
-// TODO: setup timeouts
-setTimeout(function() {
-    console.log(`Playing ${output[0].filename} at ${new Date(Date.now())}`);
-    fs.createReadStream(`audio/${output[0].filename}`)
-      .pipe(new lame.Decoder())
-      .on("format", function (format) {
-        this.pipe(new Speaker(format));
-      })
-      //.on("start", function() {console.log(`${output[0].filename} now playing`);}) 
-      .on("end", function() {console.log(`${output[0].filename} finished playing`);});
-    }, delay);
+function queueSong(song) {
+    // Set delay to 5 seconds if song is past due
+    if (Date.now() > new Date(song.timetoplay)) {
+        delay = 5000;
+    } else { // Set delay for when future song is scheduled
+        delay = new Date(song.timetoplay) - Date.now();    
+    }
+    console.log(`${song.filename} will play in ${delay/1000} seconds`); 
 
-// TODO: Allow for "Church is out" button
+    setTimeout(function() {
+        console.log(`Playing ${song.filename} at ${new Date(Date.now())}`);
+        fs.createReadStream(`audio/${song.filename}`)
+          .pipe(new lame.Decoder())
+          .on("format", function (format) {
+            this.pipe(new Speaker(format));
+          })
+          //.on("start", function() {console.log(`${output[0].filename} now playing`);}) 
+          .on("end", function() {
+            console.log(`${song.filename} finished playing`);
+            // Remove song that was just played
+            output.shift();
+            // After removing that song, queue next song if available
+            if (output.length > 0) {
+                queueSong(output[0]);
+            } else {
+                console.log("Last song just played");
+            }
+          });
+        }, delay);
+}
+    // TODO: Allow for "Church is out" button    
