4864a9c1b8b63973426b00b7f64b31050a44a86d
Zach Sanderson <zach@zachsanderson.com>
2016-10-24 16:04:57 -0400

    Added rough prompt-ability to generatearminius.js

--- generatearminius.js
+++ generatearminius.js
@@ -1,9 +1,5 @@
-//Constants
-var MININTERVALS = 100;
-var MAXINTERVALS = 200;
-var STEPBACKMINUTES = 60;
-var MININTERVALMINUTES = 10;
-var MAXINTERVALMINUTES = 20;
+// Constants
+
 var SONGLIST = [
         {filename: "057-high.mp3", songtitle: "O, For a Thousand Tongues to Sing"},
         {filename: "061-high.mp3", songtitle: "Come Thou Almighty King"},
@@ -39,37 +35,142 @@ var SONGLIST = [
         {filename: "WhisperingHope.mp3", songtitle: "Whispering Hope"}
     ];
 // Rounding coefficient for even minutes # of minutes * 60 seconds * 1000 milliseconds
-// ROUNDER could also be added to the interval portion if necessary
-var ROUNDER = 1 * 60000;
+// rounder could also be added to the interval portion if necessary
+var DEFAULTROUNDER = 1;
+var DEFAULTMININTERVALS = 100;
+var DEFAULTMAXINTERVALS = 200;
+var DEFAULTSTEPBACKMINUTES = 60;
+var DEFAULTMININTERVALMINUTES = 10;
+var DEFAULTMAXINTERVALMINUTES = 20;
+var DEFAULTMINNUMSONGS = 3;
+var DEFAULTMAXNUMSONGS = 5;
+var rounder;
+var minIntervals;
+var maxIntervals;
+var stepBackMinutes;
+var minIntervalMinutes;
+var maxIntervalMinutes;
+var minNumSongs;
+var maxNumSongs;
 
-//Variables
-var fs = require("fs");
-var numberOfIntervals = Math.random() * (MAXINTERVALS - MININTERVALS) + MININTERVALS;
-//intervalTime rounds to the nearest ROUNDER (after a # of seconds for BUFFER added)
-var intervalTime = new Date(Math.round((Date.now()) / ROUNDER) * ROUNDER);
-var songNumber;
-var csv = "";
+//Setting up prompt for customized variables
+var prompt = require('prompt');
 
-//Check initial time variable
-console.log(`Exact time:   ${new Date(Date.now())}`);
-console.log(`Rounded time: ${intervalTime.toString()}`);
-
-//Step back a few minutes to potentially generate part of playlist before Now
-intervalTime.setMinutes(intervalTime.getMinutes() - STEPBACKMINUTES);
-
-
-for (var i = 0; i < numberOfIntervals; i++) { 
-    intervalTime.setMinutes(intervalTime.getMinutes() + (Math.floor(Math.random() * (MAXINTERVALMINUTES - MININTERVALMINUTES + 1)) + MININTERVALMINUTES));
-    console.log(`Interval #${i + 1}:  ${intervalTime.toString()}`);
-    for (var j = 0; j < (Math.floor(Math.random() * 3) + 1); j++) {
-        songNumber = (Math.floor(Math.random() * SONGLIST.length));
-        console.log(`                Song #${j+1}: ${SONGLIST[songNumber].filename}`);
-        csv += `${intervalTime.toString()} | ${SONGLIST[songNumber].filename} | ${SONGLIST[songNumber].songtitle}\n`;
+//Currently the 'pattern' does not control number of digits but it ain't high priority
+//Also need to compare that mins are not larger than maxes
+var promptSchema =  {
+    properties: {
+        minIntervals: {
+            description: 'Minimum number of playback groups (1-999)',
+            type: 'integer',
+            pattern: /[1-9][0-9]{0,2}/,
+            message: 'Must be between 1 and 999',
+            default: DEFAULTMININTERVALS,
+            required: true
+        },
+        maxIntervals: {
+            description: 'Maximum number of playback groups (1-999)',
+            type: 'integer',
+            pattern: /[1-9][0-9]{0,2}/,
+            message: 'Must be between 1 and 999',
+            default: DEFAULTMAXINTERVALS,
+            required: true
+        },
+        rounder: {
+            description: 'Minutes to round to (1-99)',
+            type: 'integer',
+            pattern: /[1-9][0-9]{0,1}/,
+            message: 'Must be between 1 and 99',
+            default: DEFAULTROUNDER,
+            required: true
+        },
+        stepBackMinutes: {
+            description: 'Number of minutes to step back to create a cushion of previous songs (1-999)',
+            type: 'integer',
+            pattern: /[1-9][0-9]{0,2}/,
+            message: 'Must be between 1 and 999',
+            default: DEFAULTSTEPBACKMINUTES,
+            required: true
+        },
+        minIntervalMinutes: {
+            description: 'Minimum number of minutes between intervals (1-999)',
+            type: 'integer',
+            pattern: /[1-9][0-9]{0,2}/,
+            message: 'Must be between 1 and 999',
+            default: DEFAULTMININTERVALMINUTES,
+            required: true
+        },
+        maxIntervalMinutes: {
+            description: 'Maximum number of minutes between intervals (1-999)',
+            type: 'integer',
+            pattern: /[1-9][0-9]{0,2}/,
+            message: 'Must be between 1 and 999',
+            default: DEFAULTMAXINTERVALMINUTES,
+            required: true
+        },
+        minNumSongs: {
+            description: 'Minimum number of songs per playback group (1-99)',
+            type: 'integer',
+            pattern: /[1-9][0-9]{0,1}/,
+            message: 'Must be between 1 and 99',
+            default: DEFAULTMINNUMSONGS,
+            required: true
+        },
+        maxNumSongs: {
+            description: 'Maximum number of songs per playback group (1-99)',
+            type: 'integer',
+            pattern: /[1-9][0-9]{0,1}/,
+            message: 'Must be between 1 and 99',
+            default: DEFAULTMAXNUMSONGS,
+            required: true
+        }
     }
 }
 
-fs.writeFile("arminius.txt", csv, function(err) {
+prompt.start();
 
+prompt.get(promptSchema, function(err, result) {
+    console.log('Minimum Intervals: ' + result.minIntervals);
+    console.log('Maximum Intervals: ' + result.maxIntervals);
+    minIntervals        = result.minIntervals;
+    maxIntervals        = result.maxIntervals;
+    rounder             = result.rounder * 60000;
+    stepBackMinutes     = result.stepBackMinutes;
+    minIntervalMinutes  = result.minIntervalMinutes;
+    maxIntervalMinutes  = result.maxIntervalMinutes;
+    minNumSongs         = result.minNumSongs;
+    maxNumSongs         = result.maxNumSongs;
+
+    //Variables
+    var fs = require("fs");
+    var numberOfIntervals = Math.random() * (maxIntervals - minIntervals) + minIntervals;
+    //intervalTime rounds to the nearest rounder
+    var intervalTime = new Date(Math.round((Date.now()) / rounder) * rounder);
+    var songNumber;
+    var csv = "";
+
+    //Check initial time variable
+    console.log(`Exact time:   ${new Date(Date.now())}`);
+    console.log(`Rounded time: ${intervalTime.toString()}`);
+
+    //Step back a few minutes to potentially generate part of playlist before Now
+    intervalTime.setMinutes(intervalTime.getMinutes() - stepBackMinutes);
+
+
+    for (var i = 0; i < numberOfIntervals; i++) { 
+        intervalTime.setMinutes(intervalTime.getMinutes() + (Math.floor(Math.random() * (maxIntervalMinutes - minIntervalMinutes + 1)) + minIntervalMinutes));
+        console.log(`Interval #${i + 1}:  ${intervalTime.toString()}`);
+        for (var j = 0; j < (Math.floor(Math.random() * maxNumSongs) + minNumSongs); j++) {
+            songNumber = (Math.floor(Math.random() * SONGLIST.length));
+            console.log(`                Song #${j+1}: ${SONGLIST[songNumber].filename}`);
+            csv += `${intervalTime.toString()} | ${SONGLIST[songNumber].filename} | ${SONGLIST[songNumber].songtitle}\n`;
+        }
+    }
+
+    fs.writeFile("arminius.txt", csv, function(err) {
+
+    });
+
+    console.log("CSV file written");
 });
 
-console.log("CSV file written");
