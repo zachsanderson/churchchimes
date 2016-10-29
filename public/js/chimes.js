4ca6888064b8ddfe95653857bf51210d9aebd470
Zach Sanderson <zach@zachsanderson.com>
2016-10-29 00:24:02 -0400

    Sending socket emit to change Now Playing

--- public/js/chimes.js
+++ public/js/chimes.js
@@ -2,8 +2,11 @@ console.log('Starting up client side websocket');
 
 var socket = io();
 socket.on('play', function(data) {
+    $('#nowplayingsong').text(data.song);
+    $('#nowplaying').show();
     console.log('The server indicated "' + data.song + '" is now playing');
 });
 socket.on('stop', function(data) {
+    $('#nowplaying').hide();
     console.log('The server indicated playback has stopped');
 });
