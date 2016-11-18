82925647d5a6da6fe716738e84a740fbc88c27e8
Steven Davis <sgdavis@bioneos.com>
2016-11-18 00:09:53 -0600

    Created initial hidden Action Buttons

--- public/js/chimes.js
+++ public/js/chimes.js
@@ -1,12 +1,32 @@
 /** 
  * Client side JS for ChimePlayer project
  */
+// Action button hide delay in ms
+const HIDE_DELAY = 5000;
+
+// Local variables
 var socket;
+var buttonResetId;
+
 // Defer setup until page is ready:
 $(function() {
   // Add Action Handlers
   $('#shutdown-btn').on('click', function(ev) {
     socket.emit('shutdown');
+  });
+  $('#restart-btn').on('click', function(ev) {
+    socket.emit('restart');
+  });
+
+  // Show action buttons on a click
+  $(document).on('click', function(ev) {
+    // Because of the fixed positioning, we have to actually slide "down" to appear
+    // up from the bottom like we wanted:
+    $('#action-btns').slideDown();
+
+    // Set (or reset) a timeout for hiding the buttons if no more events occur
+    clearTimeout(buttonResetId);
+    buttonResetId = setTimeout(function() { $('#action-btns').slideUp(); }, HIDE_DELAY);
   });
 
   // Handler Websocket setup
